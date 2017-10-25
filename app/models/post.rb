94ff73c77b5cb332ef8e6c2b9f355866f505372f
aisayo <aisayo@icloud.com>
2017-10-25 00:52:57 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,13 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+
+  BAITS = [/Won't Believe/i, /Secret/i, /Top [0-9]*/i, /Guess/i]
+  validate :has_bait?
+
+  def has_bait?
+    if !BAITS.find{|b| b.match title}
+      errors.add(:title, "add click-bait to title")
+    end
+  end
 end
